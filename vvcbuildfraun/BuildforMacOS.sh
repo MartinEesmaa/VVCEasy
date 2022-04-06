686cbe2eee48296edb685356b89ed5adc7fdd3e4
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-04-06 17:39:55 +1000

    Added Mac OS build shell

---
+++ vvcbuildfraun/BuildforMacOS.sh
@@ -0,0 +1,14 @@
+echo Martin Eesmaa / VVC Compiler (vvenc and vvdec)
+echo Please make sure your Xcode is installed on your Applications.
+echo.
+cd /Home/Downloads/
+git clone https://github.com/fraunhoferhhi/vvenc.git
+git clone https://github.com/fraunhoferhhi/vvdec.git
+cd vvenc
+make release
+cd ../
+cd vvdec
+make release
+cd ../
+echo Please see the build files following starting with vvenc/bin/release-shared
+pause
