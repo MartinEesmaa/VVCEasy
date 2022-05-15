36c2a34cd4a5f6761fbb74eec0fa906312d0eab5
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-05-15 14:21:11 +1000

    remove suspend
    reason it can stop terminal

--- libvvdecffmpeg.sh
+++ libvvdecffmpeg.sh
@@ -16,4 +16,3 @@ echo Three packages are finished. One more thing to do is type code to finish it
 echo export LD_LIBRARY_PATH=~/Downloads/vvdec/install/lib
 echo Once you typed code, you're ready to preview VVC, view VVC information or convert from VVC using ffmpeg, ffplay and ffprobe.
 echo - Martin Eesmaa
-suspend
