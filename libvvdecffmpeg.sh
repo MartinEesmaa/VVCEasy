6d20aeadd3bf1ff2dbfe924cd2d43ca825c90bf3
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-05-15 14:16:53 +1000

    libvvdec on FFmpeg support

---
+++ libvvdecffmpeg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install openssl libxml2 /y
+echo Downloading libvvdec...
+cd ~/Downloads
+git clone https://github.com/fraunhoferhhi/vvdec.git
+cd vvdec
+make release && make install
+cd ../
+echo Downloading FFmpeg with VVC support
+git clone https://github.com/lehmann-c/FFmpeg.git
+cd FFmpeg
+./configure --enable-libvvdec --enable-pic --enable-openssl --enable-libxml2
+make -j
+echo Three packages are finished. One more thing to do is type code to finish it:
+echo export LD_LIBRARY_PATH=~/Downloads/vvdec/install/lib
+echo Once you typed code, you're ready to preview VVC, view VVC information or convert from VVC using ffmpeg, ffplay and ffprobe.
+echo - Martin Eesmaa
+suspend
