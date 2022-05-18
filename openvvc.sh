1559726c03620acd51b02576a4bd07713c33c02b
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-05-18 19:55:40 +1000

    improved added

--- openvvc.sh
+++ openvvc.sh
@@ -12,8 +12,9 @@ echo Downloading FFmpeg with OpenVVC support...
 git clone https://github.com/OpenVVC/FFmpeg.git
 cd FFmpeg
 echo Configuring and making of FFmpeg with OpenVVC support...
-./configure && make -j
-echo Three packages are finished. One more thing to do is type code to finish it:
+echo One more thing to do is type code to finish it:
 echo export LD_LIBRARY_PATH=~/Downloads/OpenVVC/OpenVVC/install/lib
+echo export PKG_CONFIG_PATH=~/Downloads/OpenVVC/OpenVVC/install/lib/pkgconfig
+echo ./configure && make -j &&
 echo Once you typed code, you're ready to preview VVC, view VVC information or convert from VVC using ffmpeg, ffplay and ffprobe.
 echo - Martin Eesmaa
