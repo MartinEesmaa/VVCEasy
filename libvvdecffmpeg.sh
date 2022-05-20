e46f477bb9dfea0a562f9f5070accf120678adfe
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-05-20 19:17:53 +1000

    added prefix of usr/local

--- libvvdecffmpeg.sh
+++ libvvdecffmpeg.sh
@@ -5,14 +5,11 @@ echo Downloading libvvdec...
 cd ~/Downloads
 git clone https://github.com/fraunhoferhhi/vvdec.git
 cd vvdec
-make release && make install
+make install install-prefix=/usr/local
 cd ../
 echo Downloading FFmpeg with VVC support
 git clone https://github.com/lehmann-c/FFmpeg.git
 cd FFmpeg
-echo One more thing to do is type of three codes to finish for:
-echo export LD_LIBRARY_PATH=~/Downloads/vvdec/install/lib
-echo export PKG_CONFIG_PATH=~/Downloads/vvdec/install/lib/pkgconfig
-echo ./configure --enable-libvvdec --enable-pic --enable-openssl --enable-libxml2 && make -j
-echo Once you typed code, you're ready to preview VVC, view VVC information or convert from VVC using ffmpeg, ffplay and ffprobe.
+./configure --enable-libvvdec --enable-pic --enable-openssl --enable-libxml2 && make -j
+echo You're ready to preview VVC, view VVC information or convert from VVC using ffmpeg, ffplay and ffprobe.
 echo - Martin Eesmaa
