8408b41a8c022b7bf9ebbc8c0ba0f02dd4cd25ac
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-05-22 22:32:20 +1000

    changed to improve build shell

--- libvvdecffmpeg.sh
+++ libvvdecffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt install openssl libxml2 /y
+sudo apt install libxml2 libsdl2-dev libx264-dev libx265-dev libvpx-dev -y
 echo Downloading libvvdec...
 cd ~/Downloads
 git clone https://github.com/fraunhoferhhi/vvdec.git
@@ -10,6 +10,6 @@ cd ../
 echo Downloading FFmpeg with VVC support
 git clone https://github.com/lehmann-c/FFmpeg.git
 cd FFmpeg
-./configure --enable-libvvdec --enable-pic --enable-openssl --enable-libxml2 && make -j
+./configure --enable-libvvdec --enable-pic --enable-libxml2 --enable-libx264 --enable-libx265 --enable-libvpx && make -j
 echo You're ready to preview VVC, view VVC information or convert from VVC using ffmpeg, ffplay and ffprobe.
 echo - Martin Eesmaa
