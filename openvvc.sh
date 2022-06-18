dd650bc9edac7e4dc86902287c7dad3d45005e64
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-06-18 21:05:31 +1000

    added big changes of openvvc shell build

--- openvvc.sh
+++ openvvc.sh
@@ -2,19 +2,16 @@
 sudo apt update && sudo apt upgrade -y
 echo Downloading OpenVVC...
 cd ~/Downloads
-mkdir OpenVVC && cd OpenVVC
-git clone https://github.com/OpenVVC/OpenVVC.git
+mkdir OpenVVCbuild && cd OpenVVCbuild
+git clone https://github.com/OpenVVC/OpenVVC
 echo Configuring, making and installing of OpenVVC on your computer...
 cd OpenVVC
 ./configure && make && sudo make install
 cd ../
 echo Downloading FFmpeg with OpenVVC support...
-git clone https://github.com/tbiat/FFmpeg
+git clone --depth=1 https://github.com/tbiat/FFmpeg
 cd FFmpeg
 echo Configuring and making of FFmpeg with OpenVVC support...
-echo One more thing to do is type code to finish it:
-echo export LD_LIBRARY_PATH=~/Downloads/OpenVVC/OpenVVC/install/lib
-echo export PKG_CONFIG_PATH=~/Downloads/OpenVVC/OpenVVC/install/lib/pkgconfig
-echo ./configure && make -j
+./configure --enable-libopenvvc && make -j
 echo Once you typed code, you're ready to preview VVC, view VVC information or convert from VVC using ffmpeg, ffplay and ffprobe.
 echo - Martin Eesmaa
