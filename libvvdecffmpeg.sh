8beb0749b1880560608359e5efc6338863429465
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-08-08 03:16:30 +0000

    added git depths and change sudo some commands

--- libvvdecffmpeg.sh
+++ libvvdecffmpeg.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo apt install libxml2 libsdl2-dev libx264-dev libx265-dev libvpx-dev libopus-dev -y
+sudo apt install libxml2 libsdl2-dev libx264-dev libx265-dev libvpx-dev libopus-dev cmake build-essential -y
 echo Downloading libvvdec...
 cd ~/Downloads
 mkdir buildforlinux
 cd buildforlinux
-git clone https://github.com/fraunhoferhhi/vvenc
-git clone https://github.com/fraunhoferhhi/vvdec
+git clone --depth=1 https://github.com/fraunhoferhhi/vvenc
+git clone --depth=1 https://github.com/fraunhoferhhi/vvdec
 cd vvenc
-make install install-prefix=/usr/local
+sudo make install
 cd ../
 cd vvdec
-make install install-prefix=/usr/local
+sudo make install
 cd ../
 echo Downloading FFmpeg with VVC support
-git clone https://github.com/tbiat/FFmpeg
+git clone --depth=10 https://github.com/tbiat/FFmpeg
 cd FFmpeg
 ./configure --enable-libvvenc --enable-libvvdec --enable-pic --enable-libxml2 --enable-libx264 --enable-libx265 --enable-libvpx --enable-libopus
 make -j
