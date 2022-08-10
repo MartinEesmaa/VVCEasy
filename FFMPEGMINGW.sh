5fb0f13967af408cff6325eca835db27ae5a6ba9
Martin Eesmaa <88035011+MartinEesmaa@users.noreply.github.com>
2022-08-10 14:08:30 +1000

    add some changes of mingw64 build

--- FFMPEGMINGW.sh
+++ FFMPEGMINGW.sh
@@ -2,21 +2,22 @@ echo Starting process of FFmpeg build with libvvenc and libvvdec...
 mkdir buildffmpegwin && cd buildffmpegwin
 git clone --depth=1 https://github.com/tbiat/FFmpeg
 git clone --depth=1 https://github.com/fraunhoferhhi/vvenc
-git clone --depth=1 https://github.com/fraunhoferhhi/vvdec
+git clone --depth=10 https://github.com/fraunhoferhhi/vvdec
 echo Starting to build vvenc...
 cd vvenc
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. -G "MinGW Makefiles"
-cmake --build . --config Release --target install
+cmake --build . --config Release --target install -j $nproc
 cd ../../
 cd vvdec
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local .. -G "MinGW Makefiles"
-cmake --build . --config Release --target install
+cmake --build . --config Release --target install -j $nproc
 cd ../../
 cd FFmpeg
-./configure --enable-gpl --enable-version3 --enable-libvvenc --enable-libvvdec --enable-pic \
---enable-libxml2 --enable-libaom --enable-libx264 --enable-libx265 --enable-libvpx --enable-libsvtav1 \
---enable-libopus --enable-librav1e --disable-w32threads && \
+export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
+./configure --enable-libfdk-aac --enable-static --enable-libvvenc --enable-libvvdec --enable-pic \
+--enable-zlib --enable-libxml2 --extra-libs='-lpthread -lm -lz' --extra-ldexeflags=-static \
+--pkg-config-flags=-static --disable-w32threads && \
 make -j
 echo FFmpeg VVC version tools are now compiled, please see buildffmpegwin/FFmpeg folder.
