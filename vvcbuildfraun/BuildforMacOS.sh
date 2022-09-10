721f30e562c9f450bfc94061004f572cfa5e862c
Martin Eesmaa <martin.eesmaa@protonmail.com>
2022-09-10 10:10:02 +1000

    fix configuration of mac os build script

--- vvcbuildfraun/BuildforMacOS.sh
+++ vvcbuildfraun/BuildforMacOS.sh
@@ -2,14 +2,18 @@
 echo Martin Eesmaa / VVC Compiler (vvenc and vvdec)
 echo Please make sure your Xcode is installed on your Applications.
 echo.
-cd /Home/Downloads/
-git clone https://github.com/fraunhoferhhi/vvenc.git
-git clone https://github.com/fraunhoferhhi/vvdec.git
+cd ~/Downloads
+git clone --depth=1 https://github.com/fraunhoferhhi/vvenc
+git clone --depth=1 https://github.com/fraunhoferhhi/vvdec
 cd vvenc
-make release
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . -j $nproc
 cd ../
 cd vvdec
-make release
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build . -j $nproc
 cd ../
-echo Please see the build files following starting with vvenc/bin/release-shared
+echo Please see the build files following starting with bin/release-static
 pause
