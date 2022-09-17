8a9d643a36ab6c7c743f0198a17c8f64b39500f3
Martin Eesmaa <martin.eesmaa@protonmail.com>
2022-09-17 12:13:54 +1000

    changed from old to new commands of linux script

--- libvvdecffmpeg.sh
+++ libvvdecffmpeg.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade -y
-sudo apt install libxml2 libsdl2-dev libx264-dev libx265-dev libvpx-dev libopus-dev cmake build-essential -y
-echo Downloading libvvdec...
-cd ~/Downloads
-mkdir buildforlinux
-cd buildforlinux
-git clone --depth=1 https://github.com/fraunhoferhhi/vvenc
-git clone --depth=1 https://github.com/fraunhoferhhi/vvdec
-cd vvenc
-sudo make install
-cd ../
-cd vvdec
-sudo make install
-cd ../
-echo Downloading FFmpeg with VVC support
-git clone --depth=10 https://github.com/tbiat/FFmpeg
-cd FFmpeg
-./configure --enable-libvvenc --enable-libvvdec --enable-pic --enable-libxml2 --enable-libx264 --enable-libx265 --enable-libvpx --enable-libopus
+sudo apt update && sudo apt upgrade -y && \
+sudo apt install build-essential cmake nasm autoconf pkg-config python3-setuptools libtool git wget && \
+git clone --depth=1 https://github.com/fraunhoferhhi/vvenc && \
+git clone --depth=1 https://github.com/fraunhoferhhi/vvdec && \
+git clone --depth=1 https://github.com/mstorsjo/fdk-aac && \
+git clone --depth=1 https://github.com/libsdl-org/SDL && \
+git clone --depth=1 https://github.com/gnome/libxml2 && \
+git clone --depth=1 https://github.com/xiph/opus && \
+git clone --depth=1 https://github.com/MartinEesmaa/FFmpeg-FixVVC && \
+wget https://gist.githubusercontent.com/MartinEesmaa/6c4c8158139e04d432f49ac005ba7528/raw/04b05601cb51eec7e5041f8a45b71caa552669c2/libvvenc.pc.in && \
+cp libvvenc.pc.in vvenc/pkgconfig/ && \
+cd vvenc && sudo make install-release install-prefix=/usr/local disable-lto=1 && \
+cd .. && \
+cd vvdec && sudo make install-release install-prefix=/usr/local disable-lto=1 && \
+cd .. && \
+cd fdk-aac && autoreconf -if && ./configure --prefix=/usr/local --enable-static --disable-shared && sudo make install -j $nproc && \
+cd .. && \
+cd libxml2 && autoreconf -if && ./configure --prefix=/usr/local --enable-static --disable-shared && sudo make install -j $nproc && \
+cd .. && \
+cd opus && autoreconf -if && ./configure --prefix=/usr/local --enable-static --disable-shared && sudo make install -j $nproc && \
+cd .. && \
+cd SDL && ./configure --prefix=/usr/local --enable-static --disable-shared && sudo make install -j $nproc && \
+cd .. && cd FFmpeg-FixVVC && \
+./configure --enable-static --pkg-config-flags="--static" --extra-ldexeflags="-static" \
+--enable-libfdk-aac --enable-libvvenc --enable-libvvdec --enable-pic \
+--enable-libxml2 --enable-libopus --enable-sdl2 && \
 make -j
 echo You're ready to preview VVC, view VVC information or convert from VVC using ffmpeg, ffplay and ffprobe.
 echo - Martin Eesmaa
