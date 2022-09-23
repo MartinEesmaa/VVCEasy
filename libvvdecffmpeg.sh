e44fe0a907c85cb0c40793002e164dda42a092a9
Martin Eesmaa <martin.eesmaa@protonmail.com>
2022-09-23 21:10:15 +1000

    same as instructions said from previous commit

--- libvvdecffmpeg.sh
+++ libvvdecffmpeg.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y && \
-sudo apt install build-essential cmake nasm autoconf pkg-config python3-setuptools libtool git wget && \
+sudo apt install build-essential cmake nasm autoconf pkg-config \
+python3-setuptools ninja-build python3-pip libtool git wget -y && \
+sudo pip3 install meson && \
 git clone --depth=1 https://github.com/fraunhoferhhi/vvenc && \
 git clone --depth=1 https://github.com/fraunhoferhhi/vvdec && \
 git clone --depth=1 https://github.com/mstorsjo/fdk-aac && \
 git clone --depth=1 https://github.com/libsdl-org/SDL && \
 git clone --depth=1 https://github.com/gnome/libxml2 && \
 git clone --depth=1 https://github.com/xiph/opus && \
+git clone --depth=1 https://code.videolan.org/videolan/dav1d && \
+git clone --depth=1 https://github.com/Netflix/vmaf && \
 git clone --depth=1 https://github.com/MartinEesmaa/FFmpeg-FixVVC && \
 wget https://gist.githubusercontent.com/MartinEesmaa/6c4c8158139e04d432f49ac005ba7528/raw/04b05601cb51eec7e5041f8a45b71caa552669c2/libvvenc.pc.in && \
 cp libvvenc.pc.in vvenc/pkgconfig/ && \
@@ -21,10 +25,14 @@ cd .. && \
 cd opus && autoreconf -if && ./configure --prefix=/usr/local --enable-static --disable-shared && sudo make install -j $nproc && \
 cd .. && \
 cd SDL && ./configure --prefix=/usr/local --enable-static --disable-shared && sudo make install -j $nproc && \
-cd .. && cd FFmpeg-FixVVC && \
+cd .. && \
+mkdir dav1d/build && cd dav1d/build && meson -Denable_docs=false -Ddefault_library=static -Dprefix=/usr/local && sudo ninja install && \
+cd ../../ && \
+mkdir vmaf/libvmaf/build && cd vmaf/libvmaf/build && meson -Denable_docs=false -Ddefault_library=static -Dprefix=/usr/local && sudo ninja install && \
+cd ../../../ && cd FFmpeg-FixVVC && \
 ./configure --enable-static --pkg-config-flags="--static" --extra-ldexeflags="-static" \
 --enable-libfdk-aac --enable-libvvenc --enable-libvvdec --enable-pic \
---enable-libxml2 --enable-libopus --enable-sdl2 && \
+--enable-libxml2 --enable-libopus --enable-libdav1d --enable-libvmaf --enable-sdl2 --ld=g++ && \
 make -j
 echo You're ready to preview VVC, view VVC information or convert from VVC using ffmpeg, ffplay and ffprobe.
 echo - Martin Eesmaa
