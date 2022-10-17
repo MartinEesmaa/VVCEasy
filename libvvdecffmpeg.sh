c711ba210003907ae961bbd46a38c0794ddae139
Martin Eesmaa <martin.eesmaa@protonmail.com>
2022-10-17 11:01:09 +1100

    removed vmaf in linux ffmpeg script

--- libvvdecffmpeg.sh
+++ libvvdecffmpeg.sh
@@ -10,7 +10,6 @@ git clone --depth=1 https://github.com/libsdl-org/SDL && \
 git clone --depth=1 https://github.com/gnome/libxml2 && \
 git clone --depth=1 https://github.com/xiph/opus && \
 git clone --depth=1 https://code.videolan.org/videolan/dav1d && \
-git clone --depth=1 https://github.com/Netflix/vmaf && \
 git clone --depth=1 https://github.com/MartinEesmaa/FFmpeg-FixVVC && \
 cd vvenc && sudo make install-release install-prefix=/usr/local disable-lto=1 && \
 cd .. && \
@@ -26,11 +25,10 @@ cd SDL && ./configure --prefix=/usr/local --enable-static --disable-shared && su
 cd .. && \
 mkdir dav1d/build && cd dav1d/build && meson -Denable_docs=false -Ddefault_library=static -Dprefix=/usr/local .. && sudo ninja install && \
 cd ../../ && \
-mkdir vmaf/libvmaf/build && cd vmaf/libvmaf/build && CFLAGS="-msse2 -mfpmath=sse -mstackrealign" meson -Denable_docs=false -Ddefault_library=static -Denable_float=true -Dbuilt_in_models=true -Dprefix=/usr/local .. && sudo ninja install && \
-cd ../../../ && cd FFmpeg-FixVVC && \
+cd FFmpeg-FixVVC && \
 ./configure --enable-static --pkg-config-flags="--static" --extra-ldexeflags="-static" \
 --enable-libfdk-aac --enable-libvvenc --enable-libvvdec --enable-pic \
---enable-libxml2 --enable-libopus --enable-libdav1d --enable-libvmaf --enable-sdl2 --ld=g++ && \
+--enable-libxml2 --enable-libopus --enable-libdav1d --enable-sdl2 && \
 make -j
 echo You're ready to preview VVC, view VVC information or convert from VVC using ffmpeg, ffplay and ffprobe.
 echo - Martin Eesmaa
